31d28a4890cab7a8d933eb8a2b7ad2669f63cb6e
Bubbliiiing <47347516+bubbliiiing@users.noreply.github.com>
2021-09-22 00:10:46 +0800

    Update summary.py

--- summary.py
+++ summary.py
@@ -9,5 +9,5 @@ from nets.yolo import YoloBody
 if __name__ == "__main__":
     # 需要使用device来指定网络在GPU还是CPU运行
     device  = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
-    m       = YoloBody([[6, 7, 8], [3, 4, 5], [0, 1, 2]], 80).to(device)
+    m       = YoloBody([[3, 4, 5], [1, 2, 3]], 80).to(device)
     summary(m, input_size=(3, 416, 416))
